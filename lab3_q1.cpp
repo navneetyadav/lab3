30a7da940543815e52e70a5c9f6007d8c8f3c980
navneetyadav <42238470+navneetyadav@users.noreply.github.com>
2018-08-17 06:16:06 +0000

    Add files via upload

--- lab3_q1.cpp
+++ lab3_q1.cpp
@@ -14,10 +14,11 @@ int q=a*b;
 int w=a-b;
 int e=a/b;
 //print  integers
-cout << c << endl;
-cout << q << endl;
-cout << w << endl;
-cout << e << endl;
+cout << "the sum is " c << endl;
+cout << "the multiplication  is " q << endl;
+cout <<"the difference is "  w << endl;
+cout << "the quotient is " e << endl;
+cout <<"the modulus is "<< a%b << endl;
 //operations on floating/double type variable
 float s=2.345;
 float f= 5.898;
@@ -31,14 +32,15 @@ double DOUBLE3=s*f;
 double DOUBLE4=s/f;
 
 //printing results of float/double operations
-cout << x << endl;
-cout << v << endl;
-cout << n << endl;
-cout << m << endl;
-cout << DOUBLE1 << endl;
-cout << DOUBLE2 << endl;
-cout << DOUBLE3 << endl;
-cout << DOUBLE4 << endl;
+cout << "the sum is " x << endl;
+cout << "the multiplication  is "v << endl;
+cout << "the difference  is " n << endl;
+cout << "quotient  is " m << endl;
+cout <<"the modulus is "<< s%f << endl;
+cout << "the sum   is " DOUBLE1 << endl;
+cout << "the multiplication " DOUBLE2 << endl;
+cout << "the quotient   is " DOUBLE3 << endl;
+cout << "the difference  is " DOUBLE4 << endl;
 
 //operations on boleans
  bool r =(a==f);
@@ -50,13 +52,16 @@ cout << y << endl;
 // declaring characters 
  char u='s';
 char var =u;
-// size of the different variables
+// finding remainders
+ 
+
 
 cout <<"the size of integer "<<sizeof(int) << endl;
 cout <<"the size of character "<<sizeof(char) <<endl;
 cout <<"the size of float "<<sizeof(float)<<endl;
 cout <<"the size of boolean type "<<sizeof(bool)<<endl;
 cout <<"the size of double type "<<sizeof(double)<<endl;
+cout <<"the modulus is "<< a%b << endl;
 
 
 
