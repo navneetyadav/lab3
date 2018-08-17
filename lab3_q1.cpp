ea3b680705d6a7e8e4b1f3bf1739e5529a1740c4
navneetyadav <42238470+navneetyadav@users.noreply.github.com>
2018-08-17 06:02:13 +0000

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,69 @@
+// introducing the library
+#include<iostream>
+using namespace std;
+// write the main function
+
+int main()
+{ 
+ //declaring integer type variable
+ int a=2;
+int b=4;
+//add two numbers
+int c=a+b ;
+int q=a*b;
+int w=a-b;
+int e=a/b;
+//print  integers
+cout << c << endl;
+cout << q << endl;
+cout << w << endl;
+cout << e << endl;
+//operations on floating/double type variable
+float s=2.345;
+float f= 5.898;
+float x=s+f;
+float v=s*f;
+float n=s-f;
+float m=s/f;
+double DOUBLE1=s+f;
+double DOUBLE2=s-f;
+double DOUBLE3=s*f;
+double DOUBLE4=s/f;
+
+//printing results of float/double operations
+cout << x << endl;
+cout << v << endl;
+cout << n << endl;
+cout << m << endl;
+cout << DOUBLE1 << endl;
+cout << DOUBLE2 << endl;
+cout << DOUBLE3 << endl;
+cout << DOUBLE4 << endl;
+
+//operations on boleans
+ bool r =(a==f);
+cout << r << endl;
+bool t=(q<f);
+cout << t << endl;
+bool y=(w<=e);
+cout << y << endl;
+// declaring characters 
+ char u='s';
+char var =u;
+// size of the different variables
+
+cout <<"the size of integer "<<sizeof(int) << endl;
+cout <<"the size of character "<<sizeof(char) <<endl;
+cout <<"the size of float "<<sizeof(float)<<endl;
+cout <<"the size of boolean type "<<sizeof(bool)<<endl;
+cout <<"the size of double type "<<sizeof(double)<<endl;
+
+
+
+
+
+
+
+
+return 0;
+}
